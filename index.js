a2d4cdfc107bc397ada069b03a4f40ff4e766a1b
MW Felker <mw@kizen.com>
2019-04-22 22:41:15 -0500

    Refactored functionality, moved into functions, now script can accept CLI arguments for input and output files

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 'use strict';
 
 const fs = require('fs');
-const trello = require('./data.json');
+const trelloData = ( process.argv[2] ? require(process.argv[2].toString()) : null );
+const outputFile = ( process.argv[3] ? process.argv[3].toString() : 'report.html' );
 
-const lists = trello.lists.filter((list) => {
-    return list.name === 'Web App - Backlog' || list.name === 'Mobile App - Backlog';
-});
+const transformCard = function(card) {
+    return {
+        id: card.id,
+        name: card.name,
+        description: card.desc,
+        url: card.shortUrl
+    };
+};
 
-lists.forEach((list) => {
-    const listId = list.id;
-    list.cards = trello.cards.filter((card) => {
-        return card.idList === listId && !card.closed;
-    }).map((card) => {
-        return {
-            id: card.id,
-            name: card.name,
-            description: card.desc,
-            url: card.shortUrl
-        }
-    });
-});
+const transformList = function(list) {
+    const filteredCards = trelloData.cards.filter((card) => {
+        return card.idList === list.id && !card.closed;
+    }); 
+    return {
+        id: list.id,
+        name: list.name,
+        cards: filteredCards.map(transformCard)
+    };
+}
 
-
-lists.forEach((list) => {
-    const stream = fs.createWriteStream(`${list.name}.md`);
+const generateHTML = function() {
+    let cardCounter = 0;
+    const lists = trelloData.lists.map(transformList);
+    const stream = fs.createWriteStream(outputFile);
     stream.once('open', function (fd) {
-        stream.write(`${list.name}\n===\n`);
-        list.cards.forEach((card) => {
-            stream.write(`\n### [${card.name}](${card.url})\n`);
-            stream.write(`${card.description}\n`);
-
+        lists.forEach((list) => {
+            stream.write(`\n\<h2>${list.name}</h2>\n`);
+            list.cards.forEach((card) => {
+                stream.write(`<p><a href="${card.url}">${card.name}</a></p>\n`);
+                cardCounter++;
+            });
         });
         stream.end();
+        console.log(`Finished! ${lists.length} lists, ${cardCounter} cards rendered to ${outputFile}`);
     });
-});
+    
+}
+
+// if we have no data, exit
+if (!trelloData) {
+    console.error('No data loaded, exiting');
+    return;
+} else {
+    // otherwise generate HTML from data
+    generateHTML();
+}
